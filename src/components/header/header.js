346e160807515e6a0d8f01c2fbfd8f9a245959a6
ROProntera <jackie1234567839@gmail.com>
2021-03-02 03:12:59 +0800

    Header Component

---
+++ src/components/header/header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Nav, Navbar, Form, FormControl, Button } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">Netflix</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">首頁</Nav.Link>
+            <Nav.Link href="#link">節目</Nav.Link>
+            <Nav.Link href="#home">電影</Nav.Link>
+            <Nav.Link href="#link">最新熱門影片</Nav.Link>
+            <Nav.Link href="#link">我的片單</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
